56790fed0292deba157b4dd60efc017ac0221521
Torrence Cole <torrencecole@Torrences-MacBook-Pro.local>
2020-02-28 23:23:57 -0500

    Minor css and json updates

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -24,20 +24,17 @@ function PortfolioItem(props) {
             <ScrollToTopOnMount />
             <section className="intro">
                 <h1 className="section__title section__title--intro">
-                    The title <strong>{thisWork.title}</strong>
+                    Project <strong>{thisWork.title}</strong>
                 </h1>
-                <p className="section__subtitle section__subtitle--intro">A short subtitle</p>
+                <p className="section__subtitle section__subtitle--intro">{thisWork.desc}</p>
                 <img src={thisWork.img} alt={thisWork.altImg} className="intro__img"/>
             </section>
             
             <div className="portfolio-item-individual">
-                <p>Voluptatibus, soluta blanditiis! Incidunt ea unde itaque illo molestiae eligendi sint culpa 
-                    nobis voluptas sapiente voluptate, magnam ipsum eius earum?</p>
+                <p>{thisWork.p1}</p>
                 <img src={thisWork.imgDetail} alt={thisWork.altImgDetail}/>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe et amet tenetur! Fugit sequi 
-                    optio corrupti fugiat ducimus consequatur incidunt?</p>
-                <p>Voluptatibus, soluta blanditiis! Incidunt ea unde itaque illo molestiae eligendi sint culpa 
-                    nobis voluptas sapiente voluptate, magnam ipsum eius earum?</p>
+                <p>{thisWork.p2}</p>
+                <p>{thisWork.p3}</p>
             </div>
 
             <div className="portfolio-item-button">
